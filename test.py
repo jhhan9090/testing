f4752ee2e6e02eacc0d72abd2bd997aae1af32c8
Han <jhhan9090@yonsei.ac.kr>
2022-07-28 22:29:26 +0900

    how to use github

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ print("test")
 
 
 print("new testing")
+
+
+# git init
+# git add .
+# git status
+# git commit -m "message"                                            - 메세지 남기기
+# git remode add origin https://github.com/jhhan9090/testing.git
+# git remote -v                                                      -연결 확인
+# git push origin master
+
