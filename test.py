bf66e10d31134dab82a86b2f674198a4c123a361
Han <jhhan9090@yonsei.ac.kr>
2022-07-28 22:36:01 +0900

    add some

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ print("new testing")
 # git status
 # git commit -m "message"                                            - 메세지 남기기
 # git remode add origin https://github.com/jhhan9090/testing.git
-# git remote -v                                                      -연결 확인
+# git remote -v                                                      - 연결 확인
 # git push origin master
 
+# git clone " http:// " folder name                                  - 코드 다운로드
